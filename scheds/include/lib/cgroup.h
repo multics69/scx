c2d9659513cb1c2ab4de8d598c1ab610a61062f6
Changwoo Min <changwoo@igalia.com>
2025-08-20 16:38:04 +0900

    lib: cgroup_bw: Add skeleton for CPU bandwidth control.
    Define key data structures (scx_cgroup_ctx, scx_cgroup_llc_ctx)
    to support CPU bandwidth control (cpu.max) in cgroup v2. In addition,
    add an API skeleton for BPF schedulers.
    
    Signed-off-by: Changwoo Min <changwoo@igalia.com>
    Signed-off-by: Emil Tsalapatis <etsal@meta.com>

---
+++ scheds/include/lib/cgroup.h
@@ -0,0 +1,115 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ * Copyright (c) 2025 Meta Platforms, Inc. and affiliates.
+ * Author: Changwoo Min <changwoo@igalia.com>
+ */
+#pragma once
+
+#include <errno.h>
+
+/**
+ * Configs for cpu.max
+ */
+struct scx_cgroup_bw_config {
+	/* The budget allocation from a parent cgroup to a child cgroup in nsec. */
+	u64		budget_p2c;
+	/* The budget allocation from a cgroup to its LLC context in nsec. */
+	u64		budget_c2l;
+	/* The number of LLC domains. LLC ID should be in [0, nr_llcs). */
+	int		nr_llcs;
+	/* verbose level */
+	int		verbose;
+};
+
+/**
+ * scx_cgroup_bw_lib_init - 
+ * @config:
+ *
+ * Returns
+ */
+int scx_cgroup_bw_lib_init(struct scx_cgroup_bw_config *config);
+
+/**
+ * scx_cgroup_bw_init - 
+ * @cgrp:
+ * @args:
+ *
+ * Returns
+ */
+int scx_cgroup_bw_init(struct cgroup *cgrp __arg_trusted, struct scx_cgroup_init_args *args __arg_trusted);
+
+/**
+ * scx_cgroup_bw_exit - 
+ * @cgrp:
+ *
+ * Returns
+ */
+int scx_cgroup_bw_exit(struct cgroup *cgrp __arg_trusted);
+
+/**
+ * scx_cgroup_bw_set - 
+ * @cgrp:
+ *
+ * Returns
+ */
+int scx_cgroup_bw_set(struct cgroup *cgrp __arg_trusted, u64 period_us, u64 quota_us, u64 burst_us);
+
+/**
+ * scx_cgroup_bw_reserve - 
+ * @cgrp:
+ * @llc_id:
+ * @slice_ns:
+ *
+ * Returns
+ */
+int scx_cgroup_bw_reserve(struct cgroup *cgrp __arg_trusted, int llc_id, u64 slice_ns);
+
+/**
+ * scx_cgroup_bw_consume - 
+ * @cgrp:
+ * @llc_id:
+ * @reserved_ns:
+ * @consumed_ns:
+ *
+ * Returns
+ */
+int scx_cgroup_bw_consume(struct cgroup *cgrp __arg_trusted, int llc_id, u64 reserved_ns, u64 consumed_ns);
+
+/**
+ * scx_cgroup_bw_put_aside - 
+ * @p:
+ * @ctx:
+ * @vtime:
+ * @cgrp:
+ * @llc_id:
+ *
+ * Returns
+ */
+int scx_cgroup_bw_put_aside(struct task_struct *p __arg_trusted, u64 ctx, u64 vtime, struct cgroup *cgrp __arg_trusted, int llc_id);
+
+/**
+ * scx_cgroup_bw_reenqueue -
+ *
+ * Returns
+ */
+int scx_cgroup_bw_reenqueue(void);
+
+/**
+ * REGISTER_SCX_CGROUP_BW_ENQUEUE_CB - Register an enqueue callback.
+ * @eqcb: A function name with a prototype of 'void fn(void * __arg_arena)'.
+ *
+ * @eqcb enqueues a task with BPF context @taskc following the BPF scheduler's
+ * regular enqueue path. @enqueue_cb will be called when a throttled cgroup
+ * becomes available again or when the cgroup is exiting for some reason.
+ * @eqcb MUST enqueue the task; otherwise, the task will be lost and
+ * never be scheduled.
+ */
+#define REGISTER_SCX_CGROUP_BW_ENQUEUE_CB(eqcb)					\
+	__hidden int scx_group_bw_enqueue_cb(u64 taskc)				\
+	{									\
+		extern int eqcb(u64);						\
+		eqcb(taskc);							\
+		return 0;							\
+	}
+
+extern int scx_group_bw_enqueue_cb(u64 taskc);
