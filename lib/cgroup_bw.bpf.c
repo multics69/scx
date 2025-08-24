686372d6734be742327ad5d0e72973fad553766e
Changwoo Min <changwoo@igalia.com>
2025-08-25 00:24:41 +0900

    lib: cgroup_bw: Implement scx_cgroup_bw_init().
    When a cgroup is initialized, the cgroup's context and its LLC contexts
    are initialized. Also, its parent now becomes non-leaf. If its parent is
    not threaded, it cannot have tasks, so we delete its LLC contexts.
    
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

--- lib/cgroup_bw.bpf.c
+++ lib/cgroup_bw.bpf.c
@@ -14,6 +14,8 @@ enum scx_cgroup_internal_consts {
 	CBW_NPERIOD			= (100ULL * 1000ULL * 1000ULL),
 	/* maximum number of scx_cgroup_llc_ctx: 2048 cgroups * 32 LLCs */
 	CBW_NR_CGRP_LLC_MAX		= (2048 * 32),
+	/* unlimited quota ("max") */
+	CBW_RUNTUME_INF			= ((u64)~0ULL),
 };
 
 /*
@@ -49,6 +51,7 @@ struct {
 	__uint(max_entries, CBW_NR_CGRP_LLC_MAX);
 } cbw_cgrp_llc_map SEC(".maps");
 
+
 /*
  * Timer to replenish time budget for all cgroups periodically.
  */
@@ -77,6 +80,27 @@ int replenish_timerfn(void *map, int *key, struct bpf_timer *timer);
 	if (cbw_config.verbose > 0)						\
 		bpf_printk("[%s:%d] " fmt, __func__, __LINE__, ##__VA_ARGS__);	\
 } while(0)
+
+/*
+ * Arithmetic helpers.
+ */
+#ifndef min
+#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
+#endif
+
+#ifndef max
+#define max(X, Y) (((X) < (Y)) ? (Y) : (X))
+#endif
+
+#ifndef clamp
+#define clamp(val, lo, hi) min(max(val, lo), hi)
+#endif
+
+static
+u64 div_round_up(u64 dividend, u64 divisor)
+{
+	return (dividend + divisor - 1) / divisor;
+}
 
 /*
  * Check if the kernel support cpu.max for scx schedulers.
@@ -129,10 +153,252 @@ int scx_cgroup_bw_lib_init(struct scx_cgroup_bw_config *config)
 	return 0;
 }
 
+static
+bool cgroup_is_threaded(struct cgroup *cgrp)
+{
+	return cgrp->dom_cgrp != cgrp;
+}
+
+static
+u64 cgroup_get_id(struct cgroup *cgrp)
+{
+	return cgrp->kn->id;
+}
+
+static
+struct scx_cgroup_ctx *cbw_get_cgroup_ctx(struct cgroup *cgrp)
+{
+	return bpf_cgrp_storage_get(&cbw_cgrp_map, cgrp, 0, 0);
+}
+
+static
+struct scx_cgroup_llc_ctx *cbw_alloc_llc_ctx(struct cgroup *cgrp,
+					     struct scx_cgroup_ctx *cgx,
+					     int llc_id)
+{
+	static const struct scx_cgroup_llc_ctx llcx0;
+	struct scx_cgroup_llc_ctx *llcx;
+	struct cgroup_llc_id key = {
+		.cgrp_id = cgroup_get_id(cgrp),
+		.llc_id = llc_id,
+	};
+
+	/* Allocate an LLC context on the map. */
+	if (bpf_map_update_elem(&cbw_cgrp_llc_map, &key, &llcx0, BPF_NOEXIST))
+		return NULL;
+
+	llcx = bpf_map_lookup_elem(&cbw_cgrp_llc_map, &key);
+	if (!llcx)
+		return NULL;
+
+	/* Create an associated BTQ. */
+	llcx->btq = (scx_atq_t *)scx_atq_create(false);
+	if (!llcx->btq) {
+		cbw_err("Fail to allocate a BTQ");
+		bpf_map_delete_elem(&cbw_cgrp_llc_map, &key);
+		return NULL;
+	}
+
+	/*
+	 * Set beduget_remaining to infinity in advance
+	 * if there is no upper bound.
+	 */
+	if (cgx->nquota_ub == CBW_RUNTUME_INF)
+		llcx->budget_remaining = CBW_RUNTUME_INF;
+
+	return llcx;
+}
+
+static
+struct scx_cgroup_llc_ctx *cbw_get_llc_ctx(struct cgroup *cgrp, int llc_id)
+{
+	struct cgroup_llc_id key = {
+		.cgrp_id = cgroup_get_id(cgrp),
+		.llc_id = llc_id,
+	};
+
+	return bpf_map_lookup_elem(&cbw_cgrp_llc_map, &key);
+}
+
+static
+long cbw_del_llc_ctx(struct cgroup *cgrp, int llc_id)
+{
+	struct cgroup_llc_id key = {
+		.cgrp_id = cgroup_get_id(cgrp),
+		.llc_id = llc_id,
+	};
+
+	return bpf_map_delete_elem(&cbw_cgrp_llc_map, &key);
+}
+
+static
+void cbw_drain_n_free_btq(scx_atq_t *btq)
+{
+	u64 pid64;
+
+	/*
+	 * Pop all the tasks in the BTQ and ask the BPF scheduler to enqueue
+	 * them to a DSQ for execution.
+	 */
+	while ((pid64 = scx_atq_pop(btq)) && can_loop) {
+		pid_t pid = (pid_t)pid64;
+		scx_group_bw_enqueue_cb(pid);
+	}
+
+	/* Note that ATQ does not provide an API to delete itself. */
+}
+
+static
+int cbw_init_llc_ctx(struct cgroup *cgrp, struct scx_cgroup_ctx *cgx)
+{
+	int i;
+
+	if (!cgx || !cgrp)
+		return -EINVAL;
+
+	bpf_for(i, 0, cbw_config.nr_llcs) {
+		struct scx_cgroup_llc_ctx *llcx;
+
+		llcx = cbw_alloc_llc_ctx(cgrp, cgx, i);
+		if (!llcx)
+			return -ENOMEM;
+	}
+	cgx->has_llcx = true;
+
+	return 0;
+}
+
+static
+void cbw_free_llc_ctx(struct cgroup *cgrp, struct scx_cgroup_ctx *cgx)
+{
+	int i;
+
+	if (!cgrp)
+		return;
+
+	if (cgx)
+		cgx->has_llcx = false;
+
+	bpf_for(i, 0, cbw_config.nr_llcs) {
+		struct scx_cgroup_llc_ctx *llcx;
+		scx_atq_t *btq;
+
+		llcx = cbw_get_llc_ctx(cgrp, i);
+		if (!llcx)
+			break;
+
+		btq = llcx->btq;
+		if (!cbw_del_llc_ctx(cgrp, i))
+			cbw_drain_n_free_btq(btq);
+	}
+}
+
+static
+int cbw_update_nquota_ub(struct cgroup *cgrp, struct scx_cgroup_ctx *cgx)
+{
+	struct cgroup *parent;
+	struct scx_cgroup_ctx *parentx;
+
+	/*
+	 * We assume that all its ancestors' nquota_ub are already updated
+	 * (e.g., pre-order traversal of the cgroup tree). Hence, we don't
+	 * need to walk up all its ancestors to get the minimum, so we compare
+	 * against its parent's nquota_ub.
+	 */
+	cgx->nquota_ub = cgx->nquota;
+	if ((cgrp->level > 1) &&
+	    (parent = bpf_cgroup_ancestor(cgrp, cgrp->level - 1))) {
+		parentx = cbw_get_cgroup_ctx(parent);
+		if (!parentx) {
+			bpf_cgroup_release(parent);
+			cbw_err("Fail to lookup a cgroup context");
+			return -ESRCH;
+		}
+
+		cgx->nquota_ub = min(cgx->nquota_ub, parentx->nquota);
+		bpf_cgroup_release(parent);
+	}
+
+	return 0;
+}
+
+static
+void cbw_set_bandwidth(struct cgroup *cgrp, struct scx_cgroup_ctx *cgx,
+		       u64 period_us, u64 quota_us, u64 burst_us)
+{
+	cgx->period = period_us * 1000;
+	cgx->period_start_clk = scx_bpf_now();
+
+	if (quota_us == CBW_RUNTUME_INF) {
+		cgx->nquota = CBW_RUNTUME_INF;
+		cgx->burst = 0;
+	} else {
+		cgx->nquota = div_round_up(quota_us * CBW_NPERIOD, period_us);
+		cgx->burst = burst_us * 1000;
+	}
+	cgx->burst_remaining = cgx->burst;
+}
+
+/**
+ * scx_cgroup_bw_init - Initialize a cgroup for CPU bandwidth control.
+ * @cgrp: cgroup being initialized.
+ * @args: init arguments, see the struct definition.
+ *
+ * Either the BPF scheduler is being loaded or @cgrp created, initialize
+ * @cgrp for CPU bandwidth control. When being loaded, cgroups are initialized
+ * in a pre-order from the root. This operation may block.
+ *
+ * Return 0 for success, -errno for failure.
+ */
 __hidden
 int scx_cgroup_bw_init(struct cgroup *cgrp __arg_trusted, struct scx_cgroup_init_args *args __arg_trusted)
 {
-	return -ENOTSUP;
+	struct scx_cgroup_ctx *cgx, *parentx;
+	struct cgroup *parent;
+
+	/*
+	 * Allocate and initialize scx_cgroup_ctx for @cgrp.
+	 *
+	 * For the cgroup directly under the root cgroup
+	 * (i.e., its level == 1), budget the full quota to itself,
+	 * so the cgroup can distribute the budget to its descendants
+	 * when requested.
+	 */
+	cgx = bpf_cgrp_storage_get(&cbw_cgrp_map, cgrp, 0,
+				   BPF_LOCAL_STORAGE_GET_F_CREATE);
+	if (!cgx) {
+		cbw_err("Failed to allocate cgroup ctx");
+		return -ENOMEM;
+	}
+
+	cbw_set_bandwidth(cgrp, cgx, args->bw_period_us, args->bw_quota_us,
+			  args->bw_burst_us);
+	cbw_update_nquota_ub(cgrp, cgx);
+	cgx->runtime_total_sloppy = 0;
+	cgx->budget_remaining = (cgrp->level == 1)? cgx->nquota : 0;
+	cgx->is_throttled = false;
+
+	/*
+	 * The parent of @cgrp becomes non-leaf. If the parent is not
+	 * threaded, it cannot have tasks. So, we should free its
+	 * per-LLC-cgroup contexts.
+	 */
+	if ((cgrp->level > 1) &&
+	    (parent = bpf_cgroup_ancestor(cgrp, cgrp->level - 1))) {
+		if (!cgroup_is_threaded(parent)) {
+			parentx = cbw_get_cgroup_ctx(parent);
+			cbw_free_llc_ctx(parent, parentx);
+		}
+		bpf_cgroup_release(parent);
+	}
+
+	/*
+	 * Create per-LLC-cgroup contexts if @cgrp can have tasks (i.e.,
+	 * a cgroup is either at the leaf level or threaded). Here, @cgrp
+	 * is at the leaf (a cgroup is a leaf until its child is created),
+	 * so we will create per-LLC-cgroup contexts anyway.
+	 */
+	return cbw_init_llc_ctx(cgrp, cgx);
 }
 
 __hidden
@@ -141,7 +407,16 @@ int scx_cgroup_bw_exit(struct cgroup *cgrp __arg_trusted)
 	return -ENOTSUP;
 }
 
+<<<<<<< HEAD
 __hidden
+=======
+/**
+ * scx_cgroup_bw_set - 
+ * @cgrp:
+ *
+ * Returns
+ */
+>>>>>>> 7c1c6ee4 (lib: cgroup_bw: Implement scx_cgroup_bw_init().)
 int scx_cgroup_bw_set(struct cgroup *cgrp __arg_trusted, u64 period_us, u64 quota_us, u64 burst_us)
 {
 	return -ENOTSUP;
