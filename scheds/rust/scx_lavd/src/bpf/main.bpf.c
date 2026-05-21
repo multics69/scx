84ecc00263ba13d5fe6c1c01b5768bb58de65f3b
Changwoo Min <changwoo@igalia.com>
2026-05-21 23:16:32 +0900

    scx_lavd: gate preemption by task weight as effective priority
    Preemption used to compare tasks by lat_cri + stopping time alone.
    lat_cri folds p->scx.weight in multiplicatively, but as a soft factor.
    This causes priority inversion; a latency-critical, low-priority task
    can make a less latency-critical, high-priority task wait.
    
    Since the task's weight (i.e., nice) typically represents the
    application designer's intention, it is better to respect it. Hence, we
    change the preemption condition in can_x_kick_y() to avoid priority
    inversion and to respect the application's assigned nice value.
    
    When task X has a higher weight than task Y (i.e., a lower nice value),
    X can preempt Y. When two tasks' weights are the same (i.e., there is no
    application designer's preference), we use the old condition of checking
    lat_cri and stopping time. When X's weight is lower than Y's, X never
    preempts Y.
    
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

--- scheds/rust/scx_lavd/src/bpf/main.bpf.c
+++ scheds/rust/scx_lavd/src/bpf/main.bpf.c
@@ -577,6 +577,7 @@ static void update_stat_for_running(struct task_struct *p,
 	 */
 	cpuc->flags = taskc->flags;
 	cpuc->lat_cri = taskc->lat_cri;
+	cpuc->weight = p->scx.weight;
 	cpuc->running_clk = now;
 	cpuc->est_stopping_clk = get_est_stopping_clk(taskc, p->scx.slice, now);
 
@@ -2085,6 +2086,7 @@ unlock_out:
 	cpuc->flags = 0;
 	cpuc->idle_start_clk = 0;
 	cpuc->lat_cri = 0;
+	cpuc->weight = 0;
 	cpuc->running_clk = 0;
 	cpuc->est_stopping_clk = SCX_SLICE_INF;
 	cpuc->prev_task_clk = scx_clock_task(cpu_id);
@@ -2113,6 +2115,7 @@ unlock_out:
 	barrier();
 
 	cpuc->lat_cri = 0;
+	cpuc->weight = 0;
 	cpuc->running_clk = 0;
 	cpuc->est_stopping_clk = SCX_SLICE_INF;
 
